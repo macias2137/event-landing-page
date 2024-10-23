c8ce77725fa50df2dd3ef9174e02d3a9d0b6e9d3
Maciek Moroz <moroz128@gmail.com>
2024-10-23 10:49:41 +0200

    change image src in EventContent

--- src/app/event-content.tsx
+++ src/app/event-content.tsx
@@ -24,7 +24,7 @@ const EVENT_CONTENT = [
     name: "Dr Herman Jeggels, MD (VU Ams), MRCP (UK), FBIH (Hon), DHM (Hon)",
     position: "Private Practice, Kuils River, South Africa",
     panel: "Presentation",
-    image: "../../public/image/jeggels-photo-min.jpg",
+    image: jeggels,
   },
   {
     title: "Organon and Nowadays Medicine",
@@ -32,7 +32,7 @@ const EVENT_CONTENT = [
     name: "Dr Sanjay Modi",
     position: "SMT.C.M.P. Homoeopathic Medical College, Mumbai",
     panel: "Presentation",
-    image: "../../public/image/modi-photo-min.jpeg",
+    image: modi,
   },
   {
     title:
@@ -43,7 +43,7 @@ const EVENT_CONTENT = [
     position:
       "Homeopathic Practitioner, educator, author, researcher, documentary filmmaker, podcaster",
     panel: "Presentation",
-    image: "../../public/vaishampayan-photo.jpg",
+    image: dr_v,
   },
 ];
 
