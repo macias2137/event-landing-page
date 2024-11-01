26297dd8671460ad1be4a23c975e814fd25050ce
Maciek Moroz <moroz128@gmail.com>
2024-11-01 10:19:53 +0100

    add EventContent2 to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import SponsoredBy from "./sponsored-by";
 import AboutEvent from "./about-event";
 import OurStats from "./our-stats";
 import EventContent from "./event-content";
+import EventContent2 from "./event-content2";
+
 import Faq from "./faq";
 
 export default function Portfolio() {
@@ -17,6 +19,8 @@ export default function Portfolio() {
       {/* <SponsoredBy /> */}
       {/* <AboutEvent /> */}
       {/* <OurStats /> */}
+      <EventContent2 />
+
       <EventContent />
       {/* <Faq /> */}
       <Footer />
